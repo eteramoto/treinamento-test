f89dd2c0e800fcc1f793a29d5cb192d62fcc4fea
Edson Teramoto <edson.teramoto@iteris.com.br>
2020-09-16 02:43:08 -0300

    Criação de função para obter evidências.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -6,10 +6,13 @@ describe('workspace-project App', () => {
 
   beforeEach(() => {
     page = new AppPage();
+    browser.manage().window().maximize();
+    browser.manage().timeouts().implicitlyWait(5000);
   });
 
   it('should display welcome message', () => {
     page.navigateTo();
+    page.pegarEvidencia('pagina-inicial');
     browser.sleep(10000);
     expect(page.getTitleText()).toEqual('Hello Test');
   });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,5 +1,7 @@
 import { browser, by, element } from 'protractor';
 
+const fs = require('fs');
+
 export class AppPage {
   navigateTo(): Promise<unknown> {
     return browser.get(browser.baseUrl) as Promise<unknown>;
@@ -8,4 +10,13 @@ export class AppPage {
   getTitleText(): Promise<string> {
     return element(by.css('app-root span.titulo-site')).getText() as Promise<string>;
   }
+
+  pegarEvidencia(nome_arquivo: string) {
+    browser.takeScreenshot().then((screenshot) => {
+      fs.writeFile('./evidencias/' + nome_arquivo +'.png', screenshot, 'base64', (err) => {
+        if (err) throw err;
+      });
+    });
+  }
+
 }
